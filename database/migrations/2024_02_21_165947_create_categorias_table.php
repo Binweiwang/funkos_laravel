6959ee16fd73d3a79ec7e52f1de93e7737345d26
Binweiwang <binweiwang1029@gmail.com>
2024-02-22 01:05:35 +0100

    feat(migrations): migration categorias

--- database/migrations/2024_02_21_165947_create_categorias_table.php
+++ database/migrations/2024_02_21_165947_create_categorias_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categorias', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('nombre');
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
