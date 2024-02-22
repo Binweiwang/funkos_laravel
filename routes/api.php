54ea6409aa5058d78ef35dae8b928976b406ef97
Binweiwang <binweiwang1029@gmail.com>
2024-02-22 01:07:40 +0100

    feat(routes): agregado api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoriasApiController;
+use App\Http\Controllers\FunkosApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// api funkos
+Route::get('funkos', [FunkosApiController::class, 'index']);
+Route::post('funkos', [FunkosApiController::class, 'store']);
+Route::get('funkos/{id}', [FunkosApiController::class, 'show']);
+Route::put('funkos/{id}', [FunkosApiController::class, 'update']);
+Route::delete('funkos/{id}', [FunkosApiController::class, 'destroy']);
+// api categorias
+Route::get('categorias', [CategoriasApiController::class, 'index']);
+Route::post('categorias', [CategoriasApiController::class, 'store']);
+Route::get('categorias/{id}', [CategoriasApiController::class, 'show']);
+Route::put('categorias/{id}', [CategoriasApiController::class, 'update']);
+Route::delete('categorias/{id}', [CategoriasApiController::class, 'destroy']);
