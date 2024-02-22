acb41aac0706089d1a1bb27d90dbb9a9fdc11711
Binweiwang <binweiwang1029@gmail.com>
2024-02-22 22:08:41 +0100

    feat(Spatie): agregado Spatie

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
@@ -15,6 +15,11 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+            resolve: {
+                alias: {
+                    '@': '/path/to/your/project/resources/js',
+                },
+            }
         }),
     ],
 });
