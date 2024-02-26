1c0ec4a3ff54c0fa66870df5e484a8c74f2fd4e0
Binweiwang <binweiwang1029@gmail.com>
2024-02-26 20:14:38 +0100

    feta(proyecto): proyecto actualizado

--- resources/js/Components/categorias/Form.vue
+++ resources/js/Components/categorias/Form.vue
@@ -34,7 +34,7 @@ defineEmits(['submit'])
                         class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                         <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">Categoria:</span>
                         <input type="text" v-model="form.nombre" id="nombre" autocomplete="nombre"
-                               class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"/>
+                               class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"/>
                     </div>
                     <InputError :message="$page.props.errors.nombre" class="mt-2"/>
                 </div>
